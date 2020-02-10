a95204d858dba19784353a7fb1e5dfbeae07bef6
V4LC1R <valcir212@gmail.com>
2020-02-09 23:30:49 -0300

    comecei a view do site

---
+++ catalogo-web/src/routes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import { isAuthenticated } from "./services/auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/welcome" component={() => <h1>Login</h1>} />
+      <Route path="/login" component={()=><h1>entrando</h1>}/>
+      <PrivateRoute exact path="/catalogo/view" component={()=><h1>ver pecas</h1>}/>
+      <PrivateRoute exact path="/catalogo/edit" component={()=><h1>editat pecas</h1>}/>
+      <PrivateRoute exact path="/catalogo/new" component={()=><h1>inserir pecas</h1>}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
